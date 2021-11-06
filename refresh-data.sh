ddc53ee2864949e929da365dff697b7d91e72561
hoangbkcntt98 <bugger485@gmail.com>
2021-11-06 10:28:32 +0700

    add-refresh-sh

---
+++ refresh-data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+php artisan migrate:fresh
+php aritsan passport:install
--- update.sh
+++ update.sh
@@ -3,3 +3,7 @@ git add .
 echo "commit name :"
 read name
 git commit -m $name 
+echo "push to server"
+git push heroku master
+echo "push to github"
+git push origin master
